952e0cb2da3dbff712147c88f47819537d889d37
Mia Yamada-Heidner <miayh@uw.edu>
2025-03-12 19:21:28 -0700

    add recently viewed recipes

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import clientPromise from 'src/clients/mongo';
 
 export default NextAuth({
   providers: [
@@ -8,4 +9,23 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  events: {
+    async signIn(message) {
+      const client = await clientPromise;
+      const db = client.db(process.env.MONGODB_DB);
+      const users = db.collection("users");
+
+      // Check if a user with this email already exists
+      const email = message.user.email;
+      const existingUser = await users.findOne({ email });
+
+      if (!existingUser) {
+        await users.insertOne({
+          email,
+          createdAt: new Date(),
+          recentlyViewedRecipes: []
+        });
+      }
+    },
+  },
 });
