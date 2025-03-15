4f1b22a4f7b392a5034c556f5fa45d6f31da3eac
czearing <czearing@outlook.com>
2025-03-15 11:57:06 -0700

    Cleaning up Nav styles

---
+++ src/components/Toolbar/NavigationLinks/NavigationLinks.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { Link as FluentLink, mergeClasses } from "@fluentui/react-components";
+import Link from "next/link";
+import { useNavigationStyles } from "./NavigationLInks.styles";
+import type { NavigationLinksProps } from "./NavigationLinks.types";
+import { navLinks } from "../constants";
+
+export const NavigationLinks: React.FC<NavigationLinksProps> = ({
+  currentPath,
+}) => {
+  const styles = useNavigationStyles();
+
+  return (
+    <>
+      {navLinks.map((link) => (
+        <Link
+          key={link.url}
+          href={link.url}
+          className={styles.nextLink}
+          passHref
+        >
+          <FluentLink
+            className={mergeClasses(
+              styles.linkStyles,
+              currentPath.includes(link.url) && styles.activeLink
+            )}
+          >
+            {link.label}
+          </FluentLink>
+        </Link>
+      ))}
+    </>
+  );
+};
