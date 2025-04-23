61b2e38556ea13ac1b537f02eec0a53a09f0eb3f
czearing <czearing@outlook.com>
2025-04-23 16:06:42 -0700

    Adding text decoder

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,13 @@
 import "@testing-library/jest-dom";
+import { TextEncoder, TextDecoder } from "util";
+
+global.TextEncoder = TextEncoder as typeof global.TextEncoder;
+global.TextDecoder = TextDecoder as typeof global.TextDecoder;
+
+const mockClient = { close: jest.fn() };
+jest.mock("src/clients/mongo", () => ({
+  __esModule: true,
+  default: Promise.resolve(mockClient),
+}));
+
+afterAll(() => mockClient.close());
